5bc9e61fa75d8c5bb4fb498354de6d2b6c28530d
Joe Ellul-Turner <joeetwork@gmail.com>
2023-11-23 20:32:22 +0000

    support for nodes core modules

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), nodePolyfills()],
 
 	optimizeDeps: {
 		include: ['@project-serum/anchor', '@solana/web3.js', 'buffer']
