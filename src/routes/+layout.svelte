bc25e9afd41dd5f051caa7fc1fec3c38eae543b8
Joe Ellul-Turner <joeetwork@gmail.com>
2023-11-23 16:37:47 +0000

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<style global>
+    @import "../app.css";
+</style>
+
+<script>
+    import Wallet from "$lib/components/wallet.svelte";
+</script>
+
+<nav
+    class="sticky top-0 left-0 flex w-full items-center justify-between bg-black p-3"
+>
+    <h1 class="text-xl text-white">The 76</h1>
+    <div class="flex items-center">
+        <Wallet />
+    </div>
+</nav>
+<main class="mx-auto max-w-3xl py-10">
+    <slot />
+</main>
